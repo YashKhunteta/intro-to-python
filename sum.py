f5ebb7437afc4f13f9b713da6c57faf8125871f3
YashKhunteta <101105245+YashKhunteta@users.noreply.github.com>
2022-03-22 01:05:02 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 22 01:00:00 2022
+
+@author: abc
+"""
+number1= input("Enter no1")
+number2 = input("engjfjndsf")
+
+sum = snumber1 + number2
+print(sum)
